7694fb0fd196cdb562c91000d667e5182123a658
mohamedkeshawy <mokeshawy@gmail.com>
2025-05-13 06:25:08 +0300

    Initial Commit

---
+++ datepicker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  datepicker
+//
+//  Created by Mohamed Keshawy on 13/05/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ datepickerTests/datepickerTests.swift
@@ -0,0 +1,17 @@
+//
+//  datepickerTests.swift
+//  datepickerTests
+//
+//  Created by Mohamed Keshawy on 13/05/2025.
+//
+
+import Testing
+@testable import datepicker
+
+struct datepickerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
