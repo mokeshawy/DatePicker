2eb3cd3dd10c5590eac9173baf8a7457fcd1d65e
mohamedkeshawy <mokeshawy@gmail.com>
2025-05-15 06:43:54 +0300

    Implement Date Picker View

--- datepicker/ViewController.swift
+++ datepicker/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var DatePicker: UIDatePicker!
+    
+    @IBOutlet weak var LiDatePreview: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func GetDateBtn(_ sender: UIButton) {
+        let currentDate = Date()
+        let userDOB = DatePicker.date
+        let calender = Calendar.current
+        let component = calender.dateComponents([Calendar.Component.year], from:userDOB,to: currentDate)
+        LiDatePreview.text = "You are \(component.year ?? 0) years old"
+    }
 }
 
